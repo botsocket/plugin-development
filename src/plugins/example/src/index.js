9827e57bba81dab5c06c794d0549a302fc4aff25
Jovan Jovanovic <jovanovicjovan921@gmail.com>
2021-07-20 14:09:12 +0200

    Update discord-sdk and add args example

--- src/plugins/example/src/index.js
+++ src/plugins/example/src/index.js
@@ -1,14 +1,30 @@
 'use strict';
 
+const Jade = require('@botsocket/jade');
+
 module.exports = {
     name: 'example',
     register: (client) => {
 
         client.command({
             name: 'example',
-            handler: (message) => {
+            handler: ({ message }) => {
 
                 return message.channel.send('Example response!');
+            },
+        });
+
+        client.command({
+            name: 'args',
+            args: ['text'],
+            validate: {
+                args: {
+                    text: Jade.string().required(),
+                },
+            },
+            handler: ({ message, args: { text } }) => {
+
+                return message.channel.send(text);
             },
         });
     },
